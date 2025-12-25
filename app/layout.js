e0dd0a59ebbce22b5a91fa60bc7d5e9228b091d1
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-24 21:39:51 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Only Up</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
