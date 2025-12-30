af8a8687a4ae72a7ae343b8db43761165090c189
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-30 12:11:37 -0600

    Update page.js

--- app/news/page.js
+++ app/news/page.js
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function NewsPage() {
   return (
     <main style={{ padding: 40 }}>
+      <Navbar />
       <h1>My News</h1>
       <p>Stock news will live here.</p>
     </main>
