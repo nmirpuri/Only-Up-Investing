6419fcf0af3fd3f1544ecfba7c61d201f9c99fed
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-30 12:16:06 -0600

    Update page.js

--- app/news/page.js
+++ app/news/page.js
@@ -1,9 +1,20 @@
-import Navbar from "../components/Navbar";
 
 export default function NewsPage() {
   return (
     <main style={{ padding: 40 }}>
-      <Navbar />
+    <nav style={{ display: "flex", gap: 20, marginBottom: 30 }}>
+  <Link href="/page" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    My Portfolio
+  </Link>
+
+  <Link href="/news" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    My News
+  </Link>
+
+  <Link href="/signin" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    Sign In
+  </Link>
+</nav>
       <h1>My News</h1>
       <p>Stock news will live here.</p>
     </main>
