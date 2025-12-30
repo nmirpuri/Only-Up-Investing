f941c01ca4b5b4fce48993187f4dad84a75e8397
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-30 12:20:08 -0600

    Update page.js

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,9 +1,19 @@
-import Navbar from "../components/Navbar";
-
 export default function SignInPage() {
   return (
     <main style={{ padding: 40 }}>
-      <Navbar />
+      <nav style={{ display: "flex", gap: 20, marginBottom: 30 }}>
+  <Link href="/page" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    My Portfolio
+  </Link>
+
+  <Link href="/news" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    My News
+  </Link>
+
+  <Link href="/signin" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
+    Sign In
+  </Link>
+</nav>
       <h1>Sign In</h1>
       <p>This is the sign-in page.</p>
     </main>
