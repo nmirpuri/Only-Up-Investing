6aaa7c630282d999d3830d52764b81cd9188a7cc
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-30 12:27:11 -0600

    Update page.js

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,19 +1,6 @@
 export default function SignInPage() {
   return (
     <main style={{ padding: 40 }}>
-      <nav style={{ display: "flex", gap: 20, marginBottom: 30 }}>
-  <Link href="/page" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
-    My Portfolio
-  </Link>
-
-  <Link href="/news" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
-    My News
-  </Link>
-
-  <Link href="/signin" style={{ textDecoration: "none", color: "#2596be", fontWeight: 600 }}>
-    Sign In
-  </Link>
-</nav>
       <h1>Sign In</h1>
       <p>This is the sign-in page.</p>
     </main>
