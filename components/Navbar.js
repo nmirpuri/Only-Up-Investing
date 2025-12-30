7f44b3a0c7c610cd7a2f2d158014f3fb61009281
Nikhil Mirpuri <ndmirpuri@gmail.com>
2025-12-30 12:10:28 -0600

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        gap: 24,
+        padding: "16px 0",
+        marginBottom: 30,
+        borderBottom: "1px solid #e5e7eb",
+      }}
+    >
+      <Link
+        href="/"
+        style={{
+          textDecoration: "none",
+          fontWeight: 600,
+          color: "#2596be",
+        }}
+      >
+        My Portfolio
+      </Link>
+
+      <Link
+        href="/news"
+        style={{
+          textDecoration: "none",
+          fontWeight: 600,
+          color: "#2596be",
+        }}
+      >
+        My News
+      </Link>
+
+      <Link
+        href="/signin"
+        style={{
+          textDecoration: "none",
+          fontWeight: 600,
+          color: "#2596be",
+        }}
+      >
+        Sign In
+      </Link>
+    </nav>
+  );
+}
